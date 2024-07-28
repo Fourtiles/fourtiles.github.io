8e3d52c8e84d8e3c717646d1c2203dd1eb2ceaba
Tim Morgan <github@timothymorgan.info>
2024-07-27 21:38:30 -0700

    Move action buttons to separate component

---
+++ src/components/game/ActionButtons.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="actions">
+    <button class="round" @click="game.shuffleTiles()">
+      <IconArrowsShuffle size="24" color="white" />
+    </button>
+
+    <button
+      id="add-found-word"
+      :disabled="game.currentWord.length === 0"
+      @click="game.addFoundWord"
+    >
+      <IconCircleCheck size="96" :color="addFoundWordColor" />
+    </button>
+
+    <button class="round" :disabled="!game.allFourtilesFound" @click="game.sortTiles()">
+      <IconSortDescending2Filled size="24" color="white" />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IconArrowsShuffle, IconCircleCheck, IconSortDescending2Filled } from '@tabler/icons-vue'
+import useGameStore from '@/stores/game'
+import { computed } from 'vue'
+
+const game = useGameStore()
+
+const addFoundWordColor = computed(() =>
+  game.currentWord.length === 0 ? 'var(--color-border)' : 'var(--color-brat)'
+)
+</script>
+
+<style scoped>
+#actions {
+  margin-top: var(--space-lg);
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+button.round {
+  background-color: var(--color-brat);
+  border-radius: 50%;
+  padding: 5px 5px 1px 5px; /* hacky way to get it centered in the button */
+}
+
+button:active:not(:disabled) {
+  transform: translate(2px, 2px);
+}
+
+button:not(#add-found-word):disabled {
+  background-color: var(--color-disabled);
+  cursor: inherit;
+}
+</style>
--- src/components/game/Tiles.vue
+++ src/components/game/Tiles.vue
@@ -1,44 +1,20 @@
 <template>
   <TransitionGroup id="tile-grid" name="tiles" tag="div">
-    <div id="current-word" key="current-word"><CurrentWord /></div>
+    <div id="current-word" key="current-word">
+      <CurrentWord />
+    </div>
     <Tile v-for="tile in game.tiles" :key="tile" :tile="tile" />
   </TransitionGroup>
-
-  <div id="actions">
-    <button class="round" @click="game.shuffleTiles()">
-      <IconArrowsShuffle size="24" color="white" />
-    </button>
-
-    <button id="add-found-word" :disabled="game.currentWord.length === 0" @click="addFoundWord">
-      <IconCircleCheck size="96" :color="addFoundWordColor" />
-    </button>
-
-    <button class="round" :disabled="!game.allFourtilesFound" @click="game.sortTiles()">
-      <IconSortDescending2Filled size="24" color="white" />
-    </button>
-  </div>
+  <ActionButtons />
 </template>
 
 <script setup lang="ts">
-import { IconArrowsShuffle, IconCircleCheck, IconSortDescending2Filled } from '@tabler/icons-vue'
 import Tile from '@/components/game/Tile.vue'
 import useGameStore from '@/stores/game'
-import { computed } from 'vue'
 import CurrentWord from '@/components/game/CurrentWord.vue'
+import ActionButtons from '@/components/game/ActionButtons.vue'
 
 const game = useGameStore()
-
-const addFoundWordColor = computed(() =>
-  game.currentWord.length === 0 ? 'var(--color-border)' : 'var(--color-brat)'
-)
-
-function addFoundWord() {
-  if (game.addFoundWord()) {
-    // some animation
-  } else {
-    // some other animation
-  }
-}
 </script>
 
 <style scoped>
@@ -62,29 +38,6 @@ function addFoundWord() {
   grid-area: current-word;
   justify-self: center;
   align-self: center;
-}
-
-#actions {
-  margin-top: var(--space-lg);
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-  align-items: center;
-}
-
-button.round {
-  background-color: var(--color-brat);
-  border-radius: 50%;
-  padding: 5px 5px 1px 5px; /* hacky way to get it centered in the button */
-}
-
-button:active:not(:disabled) {
-  transform: translate(2px, 2px);
-}
-
-button:not(#add-found-word):disabled {
-  background-color: var(--color-disabled);
-  cursor: inherit;
 }
 
 .tiles-move, /* apply transition to moving elements */
